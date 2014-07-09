43134640eae200919b9b38cadf2fb60c7aa25765
Yatharth Saraf <ysaraf@twitter.com>
2014-07-08 17:51:35 -0700

    Cleaning up tokens

--- java/phonetic_converter/src/PhoneticConverterTool.java
+++ java/phonetic_converter/src/PhoneticConverterTool.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.codec.language.DoubleMetaphone;
 
@@ -20,31 +22,50 @@ public class PhoneticConverterTool {
     doubleMetaphone.setMaxCodeLen(6);
 
     String filename = args[0];
-    Map<String, List<String>> encodingsToTokens = new HashMap<String, List<String>>();
+    Map<String, Set<String>> encodingsToTokens = new HashMap<String, Set<String>>();
+    Map<String, String> tokenEncodings = new HashMap<String, String>();
     BufferedReader br = new BufferedReader(new FileReader(filename));
     String line;
     while ((line = br.readLine()) != null) {
       String[] parts = line.split("\t");
       String query =  parts[0];
+      if (query == null || query.isEmpty()) {
+        continue;
+      }
+      query = URLDecoder.decode(query, StandardCharsets.UTF_8.name());
+      query = query.replaceAll("\"", "");
       String[] tokens = query.split(" ");
       for (String token : tokens) {
-        String encoding = doubleMetaphone.doubleMetaphone(token);
-        //System.out.println(query + "\t" + encoding);
-        List<String> list = encodingsToTokens.get(encoding);
-        if (list == null) {
-          list = new ArrayList<String>();
-          encodingsToTokens.put(encoding, list);
+        if (token.length() < 2) {
+          continue;
         }
-        list.add(token);
+        String encoding = doubleMetaphone.doubleMetaphone(token);
+        if (encoding == null || encoding.isEmpty()) {
+          continue;
+        }
+        Set<String> currentTokens = encodingsToTokens.get(encoding);
+        if (currentTokens == null) {
+          currentTokens = new HashSet<String>();
+          encodingsToTokens.put(encoding, currentTokens);
+        }
+        currentTokens.add(token);
+        if (!tokenEncodings.containsKey(token)) {
+          tokenEncodings.put(token, encoding);
+        }
       }
     }
     br.close();
 
     System.out.println("Encodings -> Tokens");
-    for (Map.Entry<String, List<String>> entry : encodingsToTokens.entrySet()) {
+    for (Map.Entry<String, Set<String>> entry : encodingsToTokens.entrySet()) {
       if (entry.getValue().size() < 2) {
         continue;
       }
+      System.out.println(entry.getKey() + "\t" + entry.getValue());
+    }
+
+    System.out.println("\nToken -> Encoding");
+    for (Map.Entry<String, String> entry : tokenEncodings.entrySet()) {
       System.out.println(entry.getKey() + "\t" + entry.getValue());
     }
   }
